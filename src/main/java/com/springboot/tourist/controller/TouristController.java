a21af083528a269403813fb000f3201e8d181af0
sohailpasha1 <157475077+sohailpasha1@users.noreply.github.com>
2024-02-05 15:42:28 +0530

    Update TouristController.java

--- src/main/java/com/springboot/tourist/controller/TouristController.java
+++ src/main/java/com/springboot/tourist/controller/TouristController.java
@@ -1,1 +1,31 @@
 
+package com.jocata.controller;
+
+import com.entity.Tourist;
+import com.exception.AgeLimitException;
+import com.service.TouristService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TouristController {
+
+    @Autowired
+    private TouristService touristService;
+
+    @GetMapping(value = "/")
+    private String test() {
+        return "<h1>Hello World!!</h1>";
+    }
+
+    @PostMapping(value = "/registration")
+    private ResponseEntity<Tourist> registerTourist(@RequestBody Tourist tourist) throws AgeLimitException {
+        tourist = touristService.saveTouristDetails(tourist);
+        return new ResponseEntity<>(tourist, HttpStatus.OK);
+    }
+}

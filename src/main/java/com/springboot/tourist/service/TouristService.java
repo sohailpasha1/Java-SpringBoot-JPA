30029517a4060c393384a9b9d87a28767a32af4e
sohailpasha1 <157475077+sohailpasha1@users.noreply.github.com>
2024-02-05 15:54:11 +0530

    Update TouristService.java

--- src/main/java/com/springboot/tourist/service/TouristService.java
+++ src/main/java/com/springboot/tourist/service/TouristService.java
@@ -1,8 +1,15 @@
 
 import com.entity.Tourist;
 import com.exception.AgeLimitException;
-
+import org.springframework.http.ResponseEntity;
+import java.util.List;
 
 public interface TouristService {
     Tourist registerTourist(Tourist tourist) throws AgeLimitException;
+    
+    void deleteTouristByIdService(Long id);
+
+    ResponseEntity<Tourist> fetchTouristByIdService(Long id);
+
+    ResponseEntity<List<Tourist>> fetchTouristByNameService(String name);
 }

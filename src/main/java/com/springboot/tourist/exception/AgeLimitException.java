1d88110ca8753d0395420c6d5a7e507d17ea8e36
sohailpasha1 <157475077+sohailpasha1@users.noreply.github.com>
2024-02-05 15:29:07 +0530

    Create AgeLimitException.java

---
+++ src/main/java/com/springboot/tourist/exception/AgeLimitException.java
@@ -0,0 +1,12 @@
+package com.exception;
+
+public class AgeLimitException extends Exception {
+
+    public AgeLimitException() {
+    }
+
+    public AgeLimitException(String msg) {
+        super(msg);
+    }
+
+}

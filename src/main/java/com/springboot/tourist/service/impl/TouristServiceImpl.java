14b84d59038133fd6ee2c3e89ddb013fa62426b8
sohailpasha1 <157475077+sohailpasha1@users.noreply.github.com>
2024-02-05 15:28:16 +0530

    Update TouristServiceImpl.java

--- src/main/java/com/springboot/tourist/service/impl/TouristServiceImpl.java
+++ src/main/java/com/springboot/tourist/service/impl/TouristServiceImpl.java
@@ -1,1 +1,28 @@
+package com.service.impl;
 
+import com.repository.TouristRepository;
+import com.entity.Tourist;
+import com.exception.AgeLimitException;
+import com.service.TouristService;
+import com.utils.ConstantUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+
+@Service
+public class TouristServiceImpl implements TouristService {
+
+    @Autowired
+    private TouristRepository touristRepo;
+
+    @Override
+    public Tourist registerTourist(Tourist tourist) throws AgeLimitException {
+        if (tourist.getAge() > 90) {
+            throw new AgeLimitException(ConstantUtils.AGE_LIMIT_MESSAGE);
+        }
+        Serializable serializable = touristRepo.save(tourist);
+        tourist.setTouristId((long) serializable);
+        return tourist;
+    }
+}

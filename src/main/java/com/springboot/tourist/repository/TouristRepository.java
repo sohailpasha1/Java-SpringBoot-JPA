ccad0c41fa6440de2b56e42fb96ff399ea1adbcf
sohailpasha1 <157475077+sohailpasha1@users.noreply.github.com>
2024-02-05 15:59:02 +0530

    Update TouristRepository.java

--- src/main/java/com/springboot/tourist/repository/TouristRepository.java
+++ src/main/java/com/springboot/tourist/repository/TouristRepository.java
@@ -1,4 +1,10 @@
 
 @Repository
 public interface TouristRepository extends JpaRepository<Tourist, Long> {
+
+  List<Tourist> findByName(String name);
+  
+  @Query(value = "select * from tourist where age= ?1",
+                nativeQuery = true)
+		List<Tourist> findByAgeNativeQuery(int age);
 }
